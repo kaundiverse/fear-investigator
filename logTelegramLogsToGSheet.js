39722995d0ad2b517ff265b8dba783351041c7e5
kaundiverse <163767657+kaundiverse@users.noreply.github.com>
2025-07-19 22:24:19 +0530

    Add files via upload

--- logTelegramLogsToGSheet.js
+++ logTelegramLogsToGSheet.js
@@ -1,8 +1,9 @@
+require('dotenv').config({ quiet: true });
 const { google } = require("googleapis");
 const credentials = require("./fear-investigator-telegrambot-0246c69c2b58.json");
 
 const auth = new google.auth.GoogleAuth({
-  credentials,
+  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
@@ -12,24 +13,24 @@ async function logToSheet(data) {
   const client = await auth.getClient();
   const sheets = google.sheets({ version: "v4", auth: client });
 
-  const values = [[
-    data.id,
-    data.is_bot,
-    data.first_name || "",
-    data.last_name || "",
-    data.username || "",
-    data.language_code || "",
-    data.message_id,
-    data.date,
-    data.chat_id,
-    data.chat_type || "",
-    data.chat_title || "",
-    data.text || "",             // <-- changed from data.prompt
-    data.bot_response || "",     // <-- changed from data.response
-    new Date().toISOString(),    // logging timestamp
-  ]];
-
- 
+  const values = [
+    [
+      data.id, // Telegram user ID
+      data.is_bot, // true/false
+      data.first_name || "",
+      data.last_name || "",
+      data.username || "",
+      data.language_code || "",
+      data.message_id, // Telegram message ID
+      data.date, // UNIX timestamp
+      data.chat_id, // Chat ID
+      data.chat_type || "", // private, group, etc.
+      data.chat_title || "",
+      data.prompt || "", // User message
+      data.response || "", // Bot reply
+      new Date().toISOString(), // Timestamp of log
+    ],
+  ]; 
 
   await sheets.spreadsheets.values.append({
     spreadsheetId: "1q_T7-iL7rw2xNX-D8G8cnsdMmes-OKDTLFrnG3-cTE8",
